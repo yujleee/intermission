7dfc383d0885123f6ce5b36a4c5cf47bdc1fce44
MarinaNam <namma123@naver.com>
2023-01-08 02:20:14 +0900

    fix. 리뷰마다 닉네임이 글 내용과 겹치지 않도록 고정

--- src/screens/MusicalDetail.jsx
+++ src/screens/MusicalDetail.jsx
@@ -9,7 +9,7 @@ export default function MusicalDetail() {
       <View>
         <BackdropImg
           style={StyleSheet.absoluteFill}
-          source={{ require: '../assets/hero.png' }}
+          source={require('../../assets/hero.png')}
         />
         <LinearGradient
           style={StyleSheet.absoluteFill}
@@ -20,6 +20,7 @@ export default function MusicalDetail() {
         </Title>
       </View>
       <Rating>⭐️8/10</Rating>
+      {/* 2열로 만들어서 api로 불러온 값은 2번째 열에 넣고 싶다 */}
       <Information>
         <Text>공연 기간 2023.1.7 ~ 2023.2.20</Text>
         <Text>공연 시간 160분</Text>
@@ -36,14 +37,27 @@ export default function MusicalDetail() {
         <TempText>리뷰 작성하기</TempText>
       </AddReview>
       <Review>
-        <Text>누가 죄인인가~!</Text>
+        <Text>
+          누가 죄인인가~! 이거 보고 국사 시험 잘 봤습니다. 감사합니다. 이번 공연
+          놓치지 말고 보세요. 캐스팅 쩐다. 내용이 많아지면 닉네임에 겹쳐질것
+          같아서 실험중입니다. 우려한 대로 약간 그렇습니다. 패딩을 주겠습니다.
+          그래도 해결되지 않았습니다. 울고있습니다. padding-bottom을 따로 주기
+          위해서 하나하나 패딩을 달리 주었습니다. 이래도 엉망인지 보겠습니다. 오
+          이젠 해결 된 것 같습니다. 다행입니다. 울음을 멈췄습니다.
+        </Text>
+        <Id>닉네임1</Id>
       </Review>
       <Review>
         <Text>김소현 진짜 좋다 노래 왜이렇게 잘해</Text>
+        <Id>닉네임2</Id>
       </Review>
       <Review>
-        <Text>명성황후를 시해한 죄~!</Text>
+        <Text>최재림 나오나요?</Text>
+        <Id>닉네임3</Id>
       </Review>
+      <MoreButton>
+        <TempText>리뷰 더보기</TempText>
+      </MoreButton>
     </Container>
   );
 }
@@ -51,7 +65,8 @@ export default function MusicalDetail() {
 const Container = styled.ScrollView``;
 
 const View = styled.View`
-  height: ${SCREEN_HEIGHT / 2.5 + 'px'};
+  /* 불러올 사진이 사이즈가 다 다르면 똑같이 적용안될 것 같다 */
+  height: ${SCREEN_HEIGHT / 1.5 + 'px'};
   justify-content: flex-end;
 `;
 const BackdropImg = styled.Image`
@@ -79,6 +94,7 @@ const Information = styled.View`
   margin-left: 30px;
   margin-right: 30px;
   margin-bottom: 30px;
+  flex-direction: row;
 `;
 const MoreButton = styled.TouchableOpacity`
   margin-left: 20px;
@@ -88,23 +104,36 @@ const MoreButton = styled.TouchableOpacity`
   border-radius: 5px;
   border-width: 1px;
   align-items: center;
+  /* display: inline-block; */
+  /* 한 줄에 꽉 차지 않고 가로 너비가 좀 줄었으면... */
 `;
 const TempText = styled.Text`
   font-size: 20px;
 `;
 const AddReview = styled.TouchableOpacity`
+  padding: 10px;
   margin-left: 20px;
   margin-right: 20px;
-  padding: 10px;
   margin-bottom: 20px;
   border-radius: 5px;
-  border-width: 1px;
+  border-width: 3px;
+  border-color: olive;
   align-items: center;
 `;
 const Review = styled.View`
   background-color: lightgray;
   flex-direction: row;
-  padding: 20px;
-  margin: 10px 20px;
+  padding-top: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-bottom: 30px;
+  margin: 20px;
   border-radius: 5px;
+`;
+const Id = styled.Text`
+  position: absolute;
+  right: 0px;
+  bottom: 0px;
+  margin-right: 10px;
+  margin-bottom: 10px;
 `;
