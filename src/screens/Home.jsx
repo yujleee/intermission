a05d4b5c01c230da471cf23377e1d04af5858c8d
LeeYujeong <yujlee242@gmail.com>
2023-01-08 16:49:09 +0900

    feat. 섹션 리스트 UI 컴포넌트 추가

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/native';
 import SwiperItem from '../components/Home/SwiperItem';
 import BoxOfficeList from '../components/Home/BoxOfficeList';
+import SectionList from '../components/Home/SectionList';
 
 export default function Home() {
   return (
@@ -9,6 +10,8 @@ export default function Home() {
         <SwiperItem />
       </SliderWrapper>
       <BoxOfficeList />
+      <SectionList title={'공연 예정'} />
+      <SectionList title={'서울에서 공연중'} />
     </HomeWrapper>
   );
 }
