3edaea5b47c175bd1a7a8ad0a19324a2e6e7b3f3
LeeYujeong <yujlee242@gmail.com>
2023-01-08 23:00:31 +0900

    Revert "Merge pull request #6 from yujleee/feature/detail"
    This reverts commit edc8f2acc81f0c01dd8069a58d413f3eafc7a7ea, reversing
    changes made to 2aa1623911480dac02bfb3f037c7357995d89c0e.

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,28 +1,9 @@
-import styled from '@emotion/native';
-import SwiperItem from '../components/Home/SwiperItem';
-import BoxOfficeList from '../components/Home/BoxOfficeList';
-import SectionList from '../components/Home/SectionList';
+import { Text, View } from 'react-native';
 
 export default function Home() {
   return (
-    <HomeWrapper>
-      <SliderWrapper>
-        <SwiperItem />
-      </SliderWrapper>
-      <BoxOfficeList />
-      <SectionList title={'공연 예정'} />
-      <SectionList title={'서울에서 공연중'} />
-    </HomeWrapper>
+    <View>
+      <Text>Home</Text>
+    </View>
   );
 }
-
-const HomeWrapper = styled.ScrollView``;
-
-const SliderWrapper = styled.View`
-  width: 100%;
-  height: 280px;
-  background-color: #101010;
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 80px;
-`;
