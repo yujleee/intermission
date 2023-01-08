3ca4968a4e82c280c03780142f500f307c2bb798
LeeYujeong <yujlee242@gmail.com>
2023-01-08 22:53:09 +0900

    Revert "Merge pull request #6 from yujleee/feature/detail"
    This reverts commit edc8f2acc81f0c01dd8069a58d413f3eafc7a7ea, reversing
    changes made to 2aa1623911480dac02bfb3f037c7357995d89c0e.

--- src/screens/ReviewDetail.jsx
+++ src/screens/ReviewDetail.jsx
@@ -1,48 +1,9 @@
-import styled from '@emotion/native';
-import ReviewCard from '../components/MusicalDetail/ReviewCard';
-import { SCREEN_HEIGHT } from '../util';
+import { Text, View } from 'react-native';
 
 export default function ReviewDetail() {
   return (
-    <Container>
-      <View>
-        <BackdropImg
-          style={StyleSheet.absoluteFill}
-          source={require('../../assets/hero.png')}
-        />
-        <LinearGradient
-          style={StyleSheet.absoluteFill}
-          colors={['transparent', 'black']}
-        />
-        <Title>영웅</Title>
-      </View>
-      <Rating>⭐️4.5/5</Rating>
-      <ReviewCard />
-    </Container>
+    <View>
+      <Text>ReviewDetail</Text>
+    </View>
   );
 }
-
-const View = styled.View`
-  /* 불러올 사진이 사이즈가 다 다르면 똑같이 적용안될 것 같다 */
-  height: ${SCREEN_HEIGHT / 1.5 + 'px'};
-  justify-content: flex-end;
-`;
-const BackdropImg = styled.Image`
-  width: 100%;
-  flex: 1;
-  display: block;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-`;
-const Title = styled.Text`
-  color: white;
-  font-size: 50px;
-  font-weight: 600;
-  margin-left: 20px;
-  margin-bottom: 20px;
-`;
-const Rating = styled.Text`
-  font-size: 20px;
-  margin: 10px 30px;
-`;
