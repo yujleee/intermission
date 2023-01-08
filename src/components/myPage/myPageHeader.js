6bb0dedcb72bc94b3f81abacf37781994de542af
2sel <yunwel0610@gmail.com>
2023-01-08 19:08:12 +0900

    mypage에 ui추가중입니다:)

---
+++ src/components/myPage/myPageHeader.js
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+export default function MyPageHeader() {
+  return (
+    <ScrollView>
+      <></>
+      <Text>프로필</Text>
+    </ScrollView>
+  );
+}
