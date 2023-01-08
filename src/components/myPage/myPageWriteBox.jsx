a4664974244b802a4119562f44f239f9e9f4b112
2sel <yunwel0610@gmail.com>
2023-01-09 05:07:41 +0900

    fix. ui style추가-display:block; 수정 필요합니다(안드로이드 이슈)

--- src/components/myPage/myPageWriteBox.jsx
+++ src/components/myPage/myPageWriteBox.jsx
@@ -1,12 +1,20 @@
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
+import styled from '@emotion/native';
+import ReviewCard from '../MusicalDetail/ReviewCard';
 
 export default function myPageWrite() {
   return (
     <ScrollView>
-      <Text>작성한 리뷰</Text>
-      <MyWrite>
-        <View></View>
-      </MyWrite>
+      <View>
+        <ReviewText>작성한 리뷰</ReviewText>
+      </View>
+      {/* <ReviewCard></ReviewCard> */}
     </ScrollView>
   );
 }
+
+const ReviewText = styled.Text`
+  display: flex;
+  margin-left: 16px;
+  font-size: 20px;
+`;
