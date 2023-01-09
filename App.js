0b92571d22d9014a27ca460b396e2a198f4c7741
LeeYujeong <yujlee242@gmail.com>
2023-01-09 10:33:56 +0900

    feat. 리액트 쿼리 설정 추가

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import Root from './src/navigation/Root';
+
+// react-query 이용을 위한 클라이언트
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Root />
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <Root />
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
