b985dee0cb89ee1916b2b5d6e59b2ded1c3c4b0c
2sel <yunwel0610@gmail.com>
2023-01-09 23:02:22 +0900

    feat.firebase add

--- src/screens/MyPage.jsx
+++ src/screens/MyPage.jsx
@@ -1,5 +1,17 @@
 import styled from '@emotion/native';
 import { db } from './index.js';
+import {
+  onSnapshot,
+  query,
+  collection,
+  doc,
+  orderBy,
+  addDoc,
+  getDoc,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+} from 'firebase/firestore';
 import 'firebase/firestore';
 import MyPageHeader from '../components/MyPage/MyPageHeader';
 import MyPageWriteBox from '../components/MyPage/MyPageWriteBox';
