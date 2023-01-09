892307907d925e633d25c6843ae66c90aabc9ebb
LeeYujeong <yujlee242@gmail.com>
2023-01-09 21:51:32 +0900

    fix. stacks, Tabs 수정, 컴포넌트명 변경

--- src/navigation/Stacks.jsx
+++ src/navigation/Stacks.jsx
@@ -1,36 +1,25 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { TouchableOpacity, Alert } from 'react-native';
-import Musicals from '../screens/Musicals';
-import SignUp from '../screens/SignUp';
-import Home from '../screens/Home';
+import { TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import SignUp from '../screens/SignUp';
 import ReviewDetail from '../screens/ReviewDetail';
+import MusicalDetail from '../screens/MusicalDetail';
 
 const Stack = createNativeStackNavigator();
 
 export default function Stacks({ navigation: { goBack } }) {
-  const goAlert = () =>
-    Alert.alert('뒤로', '나가시겠습니까?', [
-      { text: '네', onPress: () => goBack() },
-      { text: '아니오', onPress: () => {} },
-    ]);
   return (
     <Stack.Navigator
       initialRouteName="Home"
       screenOptions={{
         headerLeft: () => (
-          <TouchableOpacity onPress={() => goAlert()}>
+          <TouchableOpacity onPress={() => goBack()}>
             <Ionicons name="arrow-back" size={24} color="black" />
           </TouchableOpacity>
         ),
       }}
     >
-      <Stack.Screen
-        name="Home"
-        component={Home}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen name="Musicals" component={Musicals} />
+      <Stack.Screen name="MusicalDetail" component={MusicalDetail} />
       <Stack.Screen name="SignUp" component={SignUp} />
       <Stack.Screen name="ReviewDetail" component={ReviewDetail} />
     </Stack.Navigator>
