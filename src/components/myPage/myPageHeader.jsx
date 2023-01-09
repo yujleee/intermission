88a8b2f9dc3832f8afbe7af235021fc6f56ef78a
2sel <yunwel0610@gmail.com>
2023-01-09 14:37:15 +0900

    fix. pull해서 생긴 버전차 수정완료!

--- src/components/myPage/myPageHeader.jsx
+++ src/components/myPage/myPageHeader.jsx
@@ -1,17 +1,25 @@
 import { Text, TextInput, View, TouchableOpacity, Image } from 'react-native';
 import { ScrollView } from 'react-native';
 import styled from '@emotion/native';
-// import ProfileImg from '../../../assets/profile_default.jpg';
+import ProfileImg from '../../../assets/profile_default.jpg';
 
 export default function MyPageHeader() {
   return (
     <PageHeader>
-      <View>{/* <ProfileImg /> */}</View>
+      <MyImage>
+        <Image
+          source={ProfileImg}
+          style={{
+            width: 160,
+            height: 160,
+            borderRadius: 100,
+          }}
+        />
+      </MyImage>
       <MyDb>
         <PageId>
           <MyId>닉네임</MyId>
         </PageId>
-
         <LogoutButton>
           <LoginButtonText>로그아웃</LoginButtonText>
         </LogoutButton>
@@ -21,25 +29,27 @@ export default function MyPageHeader() {
 }
 
 const PageHeader = styled.View`
-  display: flex;
+  flex-wrap: wrap;
   height: 200px;
 `;
 const MyDb = styled.View`
-  display: flex;
+  margin-top: 60px;
 `;
-// const ProfileImage = styled.img``;
+const MyImage = styled.View`
+  margin: 25px;
+`;
 
-const PageId = styled.View``;
-
+const PageId = styled.Text``;
 const MyId = styled.Text`
   font-size: 20px;
   margin-top: 40px;
 `;
 
 const LogoutButton = styled.TouchableOpacity`
-  display: flex;
+  justify-content: center;
   width: 110px;
   height: 40px;
+  margin-top: 10px;
   background-color: #22affc;
 `;
 
