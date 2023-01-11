f5b156b822716a6b43d62a4147fd5e7946747f5a
MarinaNam <namma123@naver.com>
2023-01-11 22:40:04 +0900

    리뷰 수정 버튼 다크모드 적용 시도

--- src/screens/ReviewDetail.jsx
+++ src/screens/ReviewDetail.jsx
@@ -9,73 +9,38 @@ import { getMusicalData } from '../api';
 export default function ReviewDetail({
   navigation: { navigate },
   route: {
-    params: { review, from, musicalId },
+    params: { review, from },
   },
 }) {
   const goToReviewEdit = () => {
     navigate('ReviewEdit', { review, from });
   };
 
-  const { data: musicalData, isLoading: isLoadingMD } = useQuery(
-    ['MusicalData', musicalId],
-    getMusicalData
-  );
-
   return (
     <Container>
-      {/* <Title>{}</Title> */}
       {/* 정보 */}
       <Row>
         <Rating>⭐️{review?.rating}/5</Rating>
         <EditBtn onPress={goToReviewEdit}>
-          <Feather name="edit" size={30} color="black" />
+          <Feather
+            name="edit"
+            size={30}
+            color={(props) => props.theme.fontColor}
+          />
         </EditBtn>
       </Row>
       <ReviewBox>
-      <Text>{review?.contents}</Text>
-      <Id>
-        {/* {review?.userId} */}
-        닉네임1
-      </Id>
+        <Text>{review?.contents}</Text>
+        <Id>
+          {/* {review?.userId} */}
+          닉네임1
+        </Id>
       </ReviewBox>
     </Container>
   );
 }
 const Container = styled.ScrollView``;
-const View = styled.View`
-  /* 불러올 사진이 사이즈가 다 다르면 똑같이 적용안될 것 같다 */
-  height: ${SCREEN_HEIGHT / 2 + 'px'};
-  justify-content: flex-end;
-`;
 
-const InfoTotalPart = styled.View`
-  flex: 1;
-`;
-const InfoPart = styled.View`
-  /* 불러올 사진이 사이즈가 다 다르면 똑같이 적용안될 것 같다 */
-  /* height: ${SCREEN_HEIGHT / 1.5 + 'px'}; */
-  /* justify-content: flex-end; */
-  flex: 1;
-`;
-const InfoImgPart = styled.View`
-  flex: 1;
-`;
-
-const BackdropImg = styled.Image`
-  width: 100%;
-  flex: 1;
-  // display: block;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-`;
-const Title = styled.Text`
-  color: white;
-  font-size: 50px;
-  font-weight: 600;
-  margin-left: 20px;
-  margin-bottom: 20px;
-`;
 const Row = styled.View`
   flex-direction: row;
   justify-content: space-between;
@@ -90,7 +55,7 @@ const Rating = styled.Text`
   margin: 10px 30px;
 `;
 const EditBtn = styled.TouchableOpacity`
-  color: ${(props) => props.theme.fontColor};
+  /* color: ${(props) => props.theme.fontColor}; */
 `;
 const ReviewBox = styled.View`
   width: 90%;
