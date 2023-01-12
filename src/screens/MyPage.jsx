1ee4419d65e269921d81f6957c15552385bad01f
chorongs <charmblues@gmail.com>
2023-01-12 20:01:31 +0900

    제발 제발

--- src/screens/MyPage.jsx
+++ src/screens/MyPage.jsx
@@ -14,8 +14,40 @@ import styled from '@emotion/native';
 // } from 'firebase/firestore';
 import MyPageHeader from '../components/MyPage/MyPageHeader';
 import MyPageWriteBox from '../components/MyPage/MyPageWriteBox';
-
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
+import { authService, dbService } from '../firebase';
+import { useEffect } from 'react';
+import { useCallback } from 'react';
+import { Pressable } from 'react-native';
 export default function MyPage() {
+  const { reset } = useNavigation();
+
+  useFocusEffect(
+    useCallback(() => {
+      if (!authService.currentUser) {
+        // 비로그인 상태에서 마이페이지 접근 시 로그인화면으로 이동하고, 뒤로가기 시 무비탭
+        // reset은 객체를 인자로 받고 객체안에 property로 index, route를 받음
+        reset({
+          index: 1,
+          routes: [
+            {
+              name: 'Tabs',
+              params: {
+                screen: 'Home', // 돌아갈 화면
+              },
+            },
+            {
+              name: 'Stacks',
+              params: {
+                screen: 'Login',
+              },
+            },
+          ],
+        });
+        return;
+      }
+    }, [])
+  );
   return (
     <MyPageWrapper
       ListHeaderComponent={
